cec58d778a7c55b2df193c8644b873aba1879e62
Peng Gu <penggu@gmail.com>
2022-03-17 20:27:15 -0700

    Add anaconda

--- install.sh
+++ install.sh
@@ -4,91 +4,98 @@
 # Build The locate database (/var/db/locate.database)
 ############################################################
 
-sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.locate.plist
+  sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.locate.plist
 
 
 ############################################################
 # MacOS System defaults
 ############################################################
 
-defaults write com.apple.finder AppleShowAllFiles YES; # show hidden files
-defaults write com.apple.dock persistent-apps -array; # remove icons in Dock
-defaults write com.apple.dock tilesize -int 36; # smaller icon sizes in Dock
-defaults write com.apple.dock autohide -bool true; # turn Dock auto-hidng on
-defaults write com.apple.dock autohide-delay -float 0; # remove Dock show delay
-defaults write com.apple.dock autohide-time-modifier -float 0; # remove Dock show delay
-defaults write com.apple.dock orientation right; # place Dock on the right side of screen
-defaults write NSGlobalDomain AppleShowAllExtensions -bool true; # show all file extensions
-killall Dock 2>/dev/null;
-killall Finder 2>/dev/null;
+  defaults write com.apple.finder AppleShowAllFiles YES; # show hidden files
+  defaults write com.apple.dock persistent-apps -array; # remove icons in Dock
+  defaults write com.apple.dock tilesize -int 36; # smaller icon sizes in Dock
+  defaults write com.apple.dock autohide -bool true; # turn Dock auto-hidng on
+  defaults write com.apple.dock autohide-delay -float 0; # remove Dock show delay
+  defaults write com.apple.dock autohide-time-modifier -float 0; # remove Dock show delay
+  defaults write com.apple.dock orientation right; # place Dock on the right side of screen
+  defaults write NSGlobalDomain AppleShowAllExtensions -bool true; # show all file extensions
+  killall Dock 2>/dev/null;
+  killall Finder 2>/dev/null;
 
 
 ############################################################
 # vim config
 ############################################################
-echo "Setting up ~/.vimrc...";
-touch ~/.vimrc;
-echo "set number" >> ~/.vimrc;
-echo "set ruler" >> ~/.vimrc;
-echo "set nowrap" >> ~/.vimrc;
-echo "highlight OverLength ctermbg=red ctermfg=white guibg=#592929" >> ~/.vimrc;
-echo "match OverLength /\\%81v.\\+/" >> ~/.vimrc;
+  echo "Setting up ~/.vimrc...";
+  touch ~/.vimrc;
+  echo "set number" >> ~/.vimrc;
+  echo "set ruler" >> ~/.vimrc;
+  echo "set nowrap" >> ~/.vimrc;
+  echo "highlight OverLength ctermbg=red ctermfg=white guibg=#592929" >> ~/.vimrc;
+  echo "match OverLength /\\%81v.\\+/" >> ~/.vimrc;
 
 
 ############################################################
 # install Xcode Command Line Tools
+# https://github.com/timsutton/osx-vm-templates/blob/ce8df8a7468faa7c5312444ece1b977c1b2f77a4/scripts/xcode-cli-tools.sh
 ############################################################
 
-# https://github.com/timsutton/osx-vm-templates/blob/ce8df8a7468faa7c5312444ece1b977c1b2f77a4/scripts/xcode-cli-tools.sh
-touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
-PROD=$(softwareupdate -l |
-  grep -o "Command Line Tools for Xcode-.*" |
-  tr -d '\n')
-softwareupdate -i "$PROD" --verbose;
+  touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
+  PROD=$(softwareupdate -l |
+    grep -o "Command Line Tools for Xcode-.*" |
+    tr -d '\n')
+  softwareupdate -i "$PROD" --verbose;
 
 
 ############################################################
 # Unattended Homebrew installation
+# https://docs.brew.sh/Installation
 ############################################################
 
-# https://docs.brew.sh/Installation
-NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+  NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 
 ############################################################
 # Install Google Chrome browser
+# https://formulae.brew.sh/cask/google-chrome
 ############################################################
 
-# https://formulae.brew.sh/cask/google-chrome
-brew install --cask google-chrome
+  brew install --cask google-chrome
 
 
 ############################################################
 # Install git
+# https://formulae.brew.sh/formula/git#default
 ############################################################
 
-# https://formulae.brew.sh/formula/git#default
-brew install git
+  brew install git
 
 
 ############################################################
 # Install gh
+# https://formulae.brew.sh/formula/gh#default
 ############################################################
 
-#https://formulae.brew.sh/formula/gh#default
-brew install gh
+  brew install gh
 
 
 ############################################################
 # Install tmux
 ############################################################
-brew install tmux
+  brew install tmux
 
 
 ############################################################
 # Install node
 ############################################################
-brew install node
+  brew install node
+
+
+############################################################
+# Install anaconda
+# https://formulae.brew.sh/cask/anaconda
+############################################################
+  brew install --cask anaconda
 
 
 ############################################################
